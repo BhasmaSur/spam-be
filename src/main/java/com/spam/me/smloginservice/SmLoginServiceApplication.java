0d8e1a95ad7f98d8d605adb79fc2486e6d5c8523
mmishra <mmishra@calibo.com>
2022-02-07 20:56:03 +0530

    v1.1

---
+++ src/main/java/com/spam/me/smloginservice/SmLoginServiceApplication.java
@@ -0,0 +1,13 @@
+package com.spam.me.smloginservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmLoginServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmLoginServiceApplication.class, args);
+	}
+
+}
